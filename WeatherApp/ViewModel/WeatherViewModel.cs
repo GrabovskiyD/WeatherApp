eaa6d88c6868135ade0aa0856db4edc363fec30f
Dmitriy Grabovskiy <dimokurl@gmail.com>
2022-08-18 22:27:50 +0300

    Added the ViewModel and implemented INitifyPropertyChanged interface

---
+++ WeatherApp/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            { 
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propetyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propetyName));
+        }
+    }
+}
